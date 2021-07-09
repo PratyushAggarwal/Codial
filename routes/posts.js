5aea27cb004a973e83b3f203b01d50aabbb509b3
Pratyush Aggarwal <pratyushaggarwal1281@gmail.com>
2021-07-09 13:52:43 +0530

    authenticate a user before posting a post && two level of check

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postcontroller = require('../controllers/post_controller');
 
-router.post('/create',postcontroller.create);
+router.post('/create', passport.checkAuthentication ,postcontroller.create);
 
 module.exports = router;
